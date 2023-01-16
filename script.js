7e11329dde7d4cfee6392cc81332dbf7d0989d98
Sylus Abel <119014867+divin3circle@users.noreply.github.com>
2023-01-16 21:22:32 +0000

    button for event listener

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ const newDiv = document.createElement('div');
 div[0].appendChild(newDiv);
 newDiv.innerText = 'The Venus Project';
 newDiv.setAttribute('class', 'title');
+const movieAlert = document.querySelectorAll('.btn');
+console.log(movieAlert[0])
+function movieDecription(){
+  alert('My Favorite Movie Franchise');
+}
+movieAlert[0].addEventListner("click", movieDecription);
